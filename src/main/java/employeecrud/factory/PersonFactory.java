7019b33fef2940dd565b21f4f30035a621166640
Mick Soudant <micksoudant@gmail.com>
2018-10-18 14:12:48 +0200

    Introduces a person factory that keeps track of id's

---
+++ src/main/java/employeecrud/factory/PersonFactory.java
@@ -0,0 +1,31 @@
+package employeecrud.factory;
+
+import employeecrud.model.Employee;
+import employeecrud.model.IPerson;
+import employeecrud.model.Person;
+
+public class PersonFactory {
+	
+	private static int incrementedId = 0;
+	
+	public static IPerson create(String type, String name) {
+		
+
+		IPerson object;
+		if(type == "employee") {			
+			object = new Employee(incrementedId, name);			
+		} else {
+			object = new Person(incrementedId, name);
+		}
+		
+		
+		incrementedId++;
+		
+		return object;		
+	}
+	
+	public static void resetIncrementedId() {
+		incrementedId = 0;
+	}
+
+}
---
+++ src/test/java/employeecrud/factory/PersonFactoryTest.java
@@ -0,0 +1,74 @@
+package employeecrud.factory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import employeecrud.model.Employee;
+import employeecrud.model.IPerson;
+import employeecrud.model.Person;
+ 
+class PersonFactoryTest {	
+	
+	
+	@Test
+	public void createEmployees() {
+		
+		IPerson personA = PersonFactory.create("employee" ,"Bob");
+		IPerson personB = PersonFactory.create("employee", "Puk");
+		IPerson personC = PersonFactory.create("employee", "Karel");
+		
+		
+		assertTrue((personA instanceof Employee));
+		
+		assertEquals(0, personA.getId());
+		assertEquals(1, personB.getId());
+		assertEquals(2, personC.getId());
+		
+		personC = null;
+		
+		IPerson personD = PersonFactory.create("employee", "Karel Junior");
+		
+		assertEquals(3, personD.getId());
+	}
+	
+	@Test
+	public void createPeople() {
+		
+		
+		IPerson personA = PersonFactory.create("Person", "Bob");
+		IPerson personB = PersonFactory.create("Person", "Puk");
+		IPerson personC = PersonFactory.create("Person", "Karel");
+		
+				
+				
+		assertTrue((personA instanceof Person));
+			
+		
+		assertEquals(0, personA.getId());
+		
+		
+		assertEquals(1, personB.getId());
+		assertEquals("Puk", personB.getName());
+		
+		
+		assertEquals(2, personC.getId());
+		
+		personC = null;
+		
+		IPerson personD = PersonFactory.create("Person", "Karel Junior");
+		
+		assertEquals(3, personD.getId());
+		
+	}
+	
+	@AfterEach
+	public void cleanUp() {
+		PersonFactory.resetIncrementedId();
+	}
+	
+	
+}
